9d9765da5870c5442788072e1e85a8fac4513965
Santosh Kumar <dssant@gmail.com>
2019-12-21 15:19:16 +0530

    Create Decision_Tree.R

---
+++ Decision_Tree.R
@@ -0,0 +1,40 @@
+data("iris")
+View(iris)
+library(C50)
+install.packages("caret")
+library(caret)
+library(lattice)
+library(ggplot2)
+# Data partion for model building and testing
+inTraininglocal <- createDataPartition(iris$Species,p=.75, list=F)
+training <- iris[inTraininglocal,]
+view(training)
+testing <- iris[-inTraininglocal,]
+#model building
+model <- C5.0(training$Species~.,data = training,trails = 5)
+# Generating the model summary
+summary(model)
+pred <- predict.C5.0(model,testing[,-5])
+a <- table(testing$Species,pred)
+View(a)
+sum(diag(a)/sum(a))
+plot(model)
+
+
+###Bagging##############################################Bagging#####
+acc<-c()
+for(i in 1:100)
+{
+  print(i)
+  inTraininglocal<-createDataPartition(iris$Species,p=.85,list=F)
+  training1<-iris[inTraininglocal,]
+  testing<-iris[-inTraininglocal,]
+
+  fittree<-C5.0(training1$Species~.,data=training1)
+  pred<-predict.C5.0(fittree,testing[,-5])
+  a<-table(testing$Species,pred)
+  acc<-c(acc,sum(diag(a))/sum(a))
+
+}
+summary(acc)
+acc
