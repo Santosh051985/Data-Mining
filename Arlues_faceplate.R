7c55145630b01a3d20d62967477bf3521ce58732
Santosh Kumar <dssant@gmail.com>
2019-12-31 16:43:21 +0530

    Create Arlues_faceplate.R

---
+++ Arlues_faceplate.R
@@ -0,0 +1,19 @@
+install.packages("arules") # install the package
+library("arules") # invoke the package
+install.packages("readxl")
+library(readxl)
+
+mydata <- read_xlsx(file.choose(),1) # Read xlsx file
+
+rules <-  apriori(as.matrix(mydata[,2:7]),parameter=list(support=0.2,confidence=0.7)) 
+?apriori
+
+inspect(rules)
+
+inspect(sort(rules, by="lift"))
+
+#inspect(head(sort(rules, by="lift")))
+qnorm(0.975)
+qnorm(0.95)
+qnorm(0.995)
+
