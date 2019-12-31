5e2cc159400675bc011dcda4f0863431bdd53104
Santosh Kumar <dssant@gmail.com>
2019-12-31 16:34:19 +0530

    Create Association_Rules.R

---
+++ Association_Rules.R
@@ -0,0 +1,35 @@
+install.packages("arules")
+data()
+library("Matrix")
+library("arules") # Used for building association rules i.e. apriori algorithm
+data("Groceries")# loading the Groceries Data
+?Groceries
+inspect(Groceries[1:20])# showing only top 10 transactions
+head(Groceries)
+#class(Groceries) # Groceries is in transactions format
+#summary(Groceries) 
+
+install.packages("arueslViz")
+library("arulesViz") # for visualizing rules
+
+# making rules using apriori algorithm 
+# Keep changing support and confidence values to obtain different rules
+?apriori
+# Building rules using apriori algorithm
+arules <- apriori(Groceries, parameter = list(support=0.002,confidence=0.6))
+arules
+inspect(head(sort(arules,by="lift"))) # to view we use inspect 
+
+# Overal quality =to chck vakues for confidence and support
+head(quality(arules))
+
+# Different Ways of Visualizing Rules
+plot(arules)
+windows()
+plot(arules,method="grouped")
+plot(arules[1:20],method = "graph") # for good visualization try plotting only few rules
+
+
+write(arules, file="a_rules.csv",sep=",")
+
+#getwd()
