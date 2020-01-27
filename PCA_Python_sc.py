9b7989066f62ec3955d52ea13f729b5b2048f621
Santosh Kumar <dssant@gmail.com>
2020-01-27 20:32:33 +0530

    Update PCA_Python_sc.py

--- PCA_Python_sc.py
+++ PCA_Python_sc.py
@@ -1,5 +1,5 @@
 
-# importing required libraries 
+# importing required modules 
 import numpy as np 
 import matplotlib.pyplot as plt 
 import pandas as pd 
@@ -11,7 +11,7 @@ dataset = pd.read_csv('wines.csv')
 X = dataset.iloc[:, 0:13].values 
 y = dataset.iloc[:, 13].values 
 
-
+# Import scikit learn modules for split the data for train and test
 from sklearn.model_selection import train_test_split 
   
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) 
